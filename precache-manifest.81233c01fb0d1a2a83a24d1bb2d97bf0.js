67949fc20cf1ced47e42dd6d62c97d6628f54298
gerardabsi <NzQzODkwOTk4NTgxOtmdXvB5Fk4cYJq9bbxoRmAzq4+f>
2020-09-29 15:48:33 +0300

    Updates

---
+++ precache-manifest.81233c01fb0d1a2a83a24d1bb2d97bf0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f81d101b0a7b8541bf3794fb97f0bd",
+    "url": "/movies_master/index.html"
+  },
+  {
+    "revision": "1878edde75111fe60b35",
+    "url": "/movies_master/static/css/2.40c68a69.chunk.css"
+  },
+  {
+    "revision": "6e377ce77f76f7736864",
+    "url": "/movies_master/static/css/main.6ac9959e.chunk.css"
+  },
+  {
+    "revision": "1878edde75111fe60b35",
+    "url": "/movies_master/static/js/2.784e189c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movies_master/static/js/2.784e189c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e377ce77f76f7736864",
+    "url": "/movies_master/static/js/main.996b3e89.chunk.js"
+  },
+  {
+    "revision": "5de84acfbd4cd75816c3",
+    "url": "/movies_master/static/js/runtime-main.4f608c5f.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/movies_master/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/movies_master/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/movies_master/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/movies_master/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/movies_master/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/movies_master/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "30adbb5e70b70ead6a5c1f4dd6686531",
+    "url": "/movies_master/static/media/close.30adbb5e.png"
+  },
+  {
+    "revision": "685ea7c69d5103763587ab6142abd3f6",
+    "url": "/movies_master/static/media/notifications.685ea7c6.svg"
+  },
+  {
+    "revision": "d561a24225731a8ce1da9557462abf86",
+    "url": "/movies_master/static/media/search.d561a242.svg"
+  }
+]);
